8ab523ba33af6dda217f8c9c16661f96b65a1236
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2024-01-20 14:30:19 +0200

    refactored code to keyframes

--- src/components/Logo/Logo-styled.jsx
+++ src/components/Logo/Logo-styled.jsx
@@ -1,28 +1,27 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const rotateAnimation = keyframes`
+  0% {
+    transform: perspective(200px) rotateY(0deg);
+  }
+
+  25% {
+    transform: perspective(200px) rotateY(360deg);
+  }
+
+  100% {
+    transform: perspective(200px) rotateY(360deg);
+  }
+`;
 
 export const LogoSvg = styled.svg`
-
   width: 80px;
   height: 80px;
   fill: currentColor;
 
-  animation-name: rotateAnimation;
+  animation-name: ${rotateAnimation};
   animation-duration: 10s;
   animation-timing-function: linear;
   animation-iteration-count: infinite;
   animation-delay: 1000ms;
-
-  @keyframes rotateAnimation {
-    0% {
-      transform: perspective(200px) rotateY(0deg);
-    }
-
-    25% {
-      transform: perspective(200px) rotateY(360deg);
-    }
-
-    100% {
-      transform: perspective(200px) rotateY(360deg);
-    }
-  }
 `;
