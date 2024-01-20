db0bf3df3e120949a08dc6b0023ab9d31639212f
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2024-01-20 19:57:32 +0200

    fix

--- src/components/Logo/Logo-styled.jsx
+++ src/components/Logo/Logo-styled.jsx
@@ -1,17 +1,11 @@
 import styled from "styled-components";
 
 export const LogoWWrap = styled.div`
-  width: 80px;
-  height: 80px;
-  -webkit-transform-style: preserve-3d;
-  transform-style: preserve-3d;
+  width: 100%;
+  height: 100%;
 `;
 
 export const LogoSvg = styled.svg`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
   width: 100%;
   height: 100%;
 
