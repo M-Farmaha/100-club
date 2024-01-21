a8309e98b9fda8a61d69496e1ab51bbe85726015
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2024-01-21 12:23:44 +0200

    rotateX change logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -6,7 +6,7 @@ import { useSpring, a } from "@react-spring/web";
 export const Logo = () => {
   const [flipped, setFlipped] = useState(false);
   const { transform } = useSpring({
-    transform: `perspective(200px) rotateY(${flipped ? 360 : 0}deg)`,
+    transform: `perspective(200px) rotateX(${flipped ? 360 : 0}deg)`,
     config: { mass: 10, tension: 200, friction: 40 },
   });
 
@@ -15,7 +15,7 @@ export const Logo = () => {
       <a.div
         style={{
           transform,
-          rotateY: "360deg",
+          rotateX: "360deg",
           width: "80px",
           height: "80px",
           transformStyle: "preserve-3d",
