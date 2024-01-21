d41077acb1a9a6a63d69ce610eefcb56933e2124
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2024-01-22 00:14:38 +0200

    fix

--- src/components/AppBar/AppBar-styled.jsx
+++ src/components/AppBar/AppBar-styled.jsx
@@ -7,9 +7,9 @@ export const Header = styled.header`
   top: 0;
   left: 0;
   right: 0;
-  z-index: 999;
+  z-index: 999; */
   background-color: rgba(255, 255, 255, 0.8);
-  backdrop-filter: blur(8px);
+  /* backdrop-filter: blur(8px);
   box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 15px 0px;
 
   border-bottom: 1px solid;
@@ -21,9 +21,9 @@ export const Header = styled.header`
     )
     1;
 
-  transition: var(--main-transition);
+  transition: var(--main-transition); */
   display: flex;
-  justify-content: center; */
+  justify-content: center;
 `;
 
 export const NavWrap = styled.nav`
