7b4ee69dfb882ba67ceebc884bb2ea45b56fb3e5
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2024-01-25 11:37:16 +0200

    count fixed

--- src/components/StatsSection/StatsSection.jsx
+++ src/components/StatsSection/StatsSection.jsx
@@ -7,7 +7,7 @@ export const StatsSection = () => {
     { name: "Турнірів за рік", count: 45, icon: "icon-stats-1" },
     { name: "Гравців у клубі", count: 100, icon: "icon-stats-2" },
     { name: "Років досвіду", count: 8, icon: "icon-stats-3" },
-    { name: "Тренерів у клубі", count: 45, icon: "icon-stats-4" },
+    { name: "Тренерів у клубі", count: 5, icon: "icon-stats-4" },
   ];
 
   return (
