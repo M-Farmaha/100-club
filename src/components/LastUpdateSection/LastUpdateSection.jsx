266bff76f3555c73b6ba4d4556a7c6fa635ca4cd
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2024-02-18 22:34:59 +0200

    last update change

--- src/components/LastUpdateSection/LastUpdateSection.jsx
+++ src/components/LastUpdateSection/LastUpdateSection.jsx
@@ -11,7 +11,7 @@ export const LastUpdateSection = () => {
               <use href={sprite + "#icon-loop"}></use>
             </IconSvg>
           </IconWrap>
-          Останнє оновлення: 18.02.2024
+          Останнє оновлення: 19.02.2024 (Подивись фото Ореста)
         </Text>
       </Section>
     </>
