0b81428a978c8d7822d31fac8d05617652b25fc3
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2025-04-12 16:58:49 +0300

    updated avatars

--- src/components/LastUpdateSection/LastUpdateSection.jsx
+++ src/components/LastUpdateSection/LastUpdateSection.jsx
@@ -11,7 +11,7 @@ export const LastUpdateSection = () => {
               <use href={sprite + "#icon-loop"}></use>
             </IconSvg>
           </IconWrap>
-          Останнє оновлення: 01.04.2025 - Оновлено дані турнірів
+          Останнє оновлення: 12.04.2025
         </Text>
       </Section>
     </>
