2c4e7bfe8045b1e057529259d9dbc485b6a8e9a2
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2025-08-29 23:29:47 +0300

    new tournaments

--- src/components/LastUpdateSection/LastUpdateSection.jsx
+++ src/components/LastUpdateSection/LastUpdateSection.jsx
@@ -11,7 +11,7 @@ export const LastUpdateSection = () => {
               <use href={sprite + "#icon-loop"}></use>
             </IconSvg>
           </IconWrap>
-          Останнє оновлення: 24.08.2025
+          Останнє оновлення: 29.08.2025
         </Text>
       </Section>
     </>
