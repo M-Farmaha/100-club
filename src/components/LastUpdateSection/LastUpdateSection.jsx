6fe3b183179f28ba25b29d88e472b161e7eb7b93
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2025-09-15 23:19:57 +0300

    updated date

--- src/components/LastUpdateSection/LastUpdateSection.jsx
+++ src/components/LastUpdateSection/LastUpdateSection.jsx
@@ -11,7 +11,7 @@ export const LastUpdateSection = () => {
               <use href={sprite + "#icon-loop"}></use>
             </IconSvg>
           </IconWrap>
-          Останнє оновлення: 12.09.2025
+          Останнє оновлення: 15.09.2025
         </Text>
       </Section>
     </>
